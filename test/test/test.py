9ae34bf649fa92b5f62d0b2a94625b1e0c0ffbc5
wzehui <wzehui@hotmail.com>
2019-03-08 17:49:36 +0100

    first test on CPU

--- test/test/test.py
+++ test/test/test.py
@@ -1,11 +1,21 @@
 import torch
 import torch.optim as optim
 from torch import nn
+import matplotlib.pyplot as plt
 # print(torch.__version__)
 
 torch.manual_seed(1)    # reproducible
-x_c = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=0)  # x data (tensor), shape=(100, 1)
-x_u = 5 * x_c + 2 + 0.2*torch.rand(x_c.size())  # noisy y data (tensor), shape=(100, 1)
+
+pi = 3.14
+omega = 1 * pi
+phi = 0.5 * pi
+A = 3
+n = torch.unsqueeze(torch.linspace(1, 10, 100), dim=1)  # x data (tensor), shape=(100, 1)
+
+x_c = A * torch.sin(omega * n + phi)
+x_u = A * torch.sin(omega * n + phi) + 0.5 * torch.randn(n.size())
+# plt.plot(n.squeeze().numpy(), x_u.squeeze().numpy(), '.')
+# plt.show()
 
 
 def model(x, w, b):
@@ -17,9 +27,9 @@ def loss_fn(x_p, x_c):
     return squared_diffs.mean()
 
 
-params = torch.tensor([1.0, 0.0], requires_grad=True)
+params = torch.tensor([1.0, 1.0], requires_grad=True)
 
-nepochs = 100
+nepochs = 500
 learning_rate = 1e-1
 
 optimizer = optim.Adam([params], lr=learning_rate)
