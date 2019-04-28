db34c15edcd77d0e625a4ac9fb5f8382da2126db
wzehui <wzehui@hotmail.com>
2019-04-28 15:45:50 +0200

    add model saving function

--- RNN_Classification/main.py
+++ RNN_Classification/main.py
@@ -35,28 +35,47 @@ def fParseConfig(sFile):
 
 
 def train(model, epoch):
+    # tells layers like dropout, batchnorm etc. that you are training the model
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+        #
+        correct = 0
         # 将求解器中的参数置零
         optimizer.zero_grad()
-        # if torch.cuda.is_available():
+        #
+        # accuracy_b = 0
         data = data.to(device)
         target = target.to(device)
-        data = data.requires_grad_()  # set requires_grad to True for training
+        # set requires_grad to True for training
+        data = data.requires_grad_()
         # 前向传播
         output = model(data)
-        output = output.permute(1, 0, 2)  # original output dimensions are batchSizex1x10
+        output = output.permute(1, 0, 2)  # original output dimensions are batchSizex1x2
+
+        # choose a larger log-probability (log-softmax)
+        # predict = torch.ge(output[0][0:, 1], output[0][0:, 0], out=None)
+        # correct = (predict.long() == target).float().sum()
+
+        # better method
+        pred = output.max(2)[1]  # get the index of the max log-probability
+        correct += pred.eq(target).cpu().sum().item()
+
+        accuracy_cur = correct / output.size()[1]   # correct / batchsize
+
         # 计算损失  The negative log likelihood loss：负对数似然损失 nll_loss
-        loss = F.nll_loss(output[0], target)  # the loss functions expects a batchSizex10 input
+        loss = F.nll_loss(output[0], target)  # the loss functions expects a batchSizex2 input
         # 损失反传
         loss.backward()
         # 优化求解
         optimizer.step()
+
         if batch_idx % log_interval == 0:  # print training stats
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss))
+            print("Accuracy: {:.3f}".format(accuracy_cur))
 
+    torch.save(model.state_dict(), '/Users/wzehui/Documents/MA/Model/bestModel')
 
 ######################################################################
 # Now that we have a training function, we need to make one for testing
@@ -101,7 +120,7 @@ else:
     test_set = PreprocessData(cfg['IndexPath'], cfg['FilePath'], cfg['TestSplitRate'])
 
 print("Train set size: " + str(len(train_set)))
-a = train_set[0]
+# a = train_set[0]
 print("Test set size: " + str(len(test_set)))
 
 kwargs = {'num_workers': 2, 'pin_memory': True} if device == 'cuda' else {}  # needed for using datasets on gpu
@@ -112,20 +131,35 @@ test_loader = torch.utils.data.DataLoader(test_set, batch_size=cfg['BatchSize'],
 # load network structure
 model = Net()
 model.to(device)
-print(model)
+# print(model)
 
 # We will use the same optimization technique used in the paper, an Adam
 # optimizer with weight decay set to 0.0001. At first, we will train with
 # a learning rate of 0.01, but we will use a ``scheduler`` to decrease it
 # to 0.001 during training.
-optimizer = optim.Adam(model.parameters(), lr = 0.01, weight_decay = 0.0001)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size = 20, gamma = 0.1)
+optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
 
+# Print model's state_dict
+print("Model's state_dict:")
+for param_tensor in model.state_dict():
+    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
 
-log_interval = 20
+# Print optimizer's state_dict
+print("Optimizer's state_dict:")
+for var_name in optimizer.state_dict():
+    print(var_name, "\t", optimizer.state_dict()[var_name])
+
+log_interval = 1
 for epoch in range(1, 11):
     if epoch == 31:
         print("First round of training complete. Setting learn rate to 0.001.")
     scheduler.step()
+    try:
+        model = torch.load(cfg['BestModelPath'])
+    except(FileNotFoundError):
+        model = Net()
+
+    model.to(device)
     train(model, epoch)
-    test(model, epoch)
+    # test(model, epoch)
